349e7f8bb462c2d04308169797e36f2e4d76d389
rtk-group <mydevclass9690@gmail.com>
2025-07-17 17:55:29 +0530

    updates in about what-we-do mission

--- frontend/src/app/(home)/about-trust/page.js
+++ frontend/src/app/(home)/about-trust/page.js
@@ -11,35 +11,21 @@ function Page() {
             <div className='pt-2 md:pt-10  pb-18 grid grid-rows lg:grid-cols-[2fr_1fr] lg:max-w-5xl mx-auto'>
                 {/* left section */}
 
-                <div className='pl-4 text-black'>
-                    <p className='text-2xl mb-2'>Details</p>
-                    <p className='text-sm'>AL-HADI EDUCATIONAL TRUST DETAILS.</p>
-                    <p className='text-xs text-gray-700 mb-2'>Umeed Global School, managed by Al Hadi Educational Trust.<br />Details of the trust are are follows:</p>
+                <div className='pl-4 text-black p-3'>
+                    <p className='text-2xl mb-2'>About Us</p>
+                    <p className='text-sm font-semibold'>Qadri Khair Foundation</p>
+                    <p className='text-xs text-gray-700 mb-2'>was established on *7th November 2024* to <br /> uplift underprivileged communities through education. It runs <br /> *Q K Academy, which provides **school education for boys and girls up to 12 years*.</p>
 
-                    <div className='p-2 lg:p-5 lg:py-10 border-l-yellow-500 border-l-5 w-full lg:max-w-130 '>
+                    <div className='p-2 lg:p-5 lg:py-7 border-l-yellow-500 border-l-5 w-full lg:max-w-130 '>
 
                         <div className='text-xs'>
-                            <p className=' '> <span className='font-semibold'>AL-HADI EDUCATIONAL TRUST</span> is a Trust registered under the provisions of the Indian Trust Act, 1882 with an objective of establishing educational institutions.</p>
 
-                            <div className='ml-3'>
-                                <p className='font-semibold mt-5'>. Vital Documents of Al Hadi Educational Trust :</p>
-                                <p className='ml-9'>Trust registration number: IV - 2060/2000</p>
-                                <p className='ml-9'>PAN No : AAITA0997L</p>
-                                <p className='ml-9'>TAN No: CALA30044A</p>
+                            <div className='ml-2 text-sm'>
 
-                                <p className='font-semibold mt-5'>. Al Hadi Educational Trust is registered under 12AA of Income Tax Act:</p>
-                                <p className='ml-9'>Registration No: CIT (exemption) Kolkata/12AA/2020-</p>
-                                <p className='ml-9'>21/A/10707 Din & Order No: ITBA/EXM/S/12AA/2020-</p>
-                                <p className='ml-9'>21/1031340048(1)</p>
-                                <p className='ml-9'>Date: 09/03/2021</p>
-
-                                <p className='font-semibold mt-5'>. Al Hadi Educational Trust is registered under 80G of Income Tax Act:</p>
-                                <p className='ml-9'>Registration No: CIT (exemption) Kolkata/80G/2020-</p>
-                                <p className='ml-9'>21/A/10573 Din & Order No: ITBA/EXM/S/80G/2020-</p>
-                                <p className='ml-9'>21/1031340235(1)</p>
-
-                                <p className='font-semibold mt-5'>. Al Hadi Educational Trust is eligible for undertaking CSR activities.</p>
-                                <p className='ml-9'>CSR No : CSR00015906</p>
+                                <p className='font-semibold'>* Medium: English & Urdu</p>
+                                <p className='font-semibold mt-1'>* Syllabus: Independent</p>
+                                <p className='font-semibold mt-1'>* Focus: Day school (non-residential)</p>
+                                <p className='font-semibold mt-1'>* Future Goal: 5-acre campus</p>
                             </div>
                         </div>
                     </div>
@@ -51,8 +37,8 @@ function Page() {
                 <div className='px-12 lg:px-0 flex justify-center lg:block mt-20 lg:mt-0 mb-3 lg:mb-0 w-full '>
 
                     <div className='w-full md:max-w-80'>
-                        <div className='py-7 px-10 shadow-[0_5px_25px_0_gray]'>
-                            <button className='text-gray-700 text-xs py-3 w-full bg-yellow-500 rounded-sm relative top-0 hover:-top-2 transition-all duration-400 cursor-pointer'>DONATE NOW</button>
+                        <div className='py-7 px-10 shadow-[0_5px_25px_0_gray] rounded-lg'>
+                          <Link href={'/donate-us'}>  <button className='text-gray-700 text-xs py-3 w-full bg-yellow-500 rounded-sm relative top-0 hover:-top-2 transition-all duration-400 cursor-pointer'>DONATE NOW</button> </Link>
                         </div>
                     </div>
 
--- frontend/src/app/(home)/vision-mission/page.js
+++ frontend/src/app/(home)/vision-mission/page.js
@@ -13,21 +13,10 @@ function Page() {
                         {/* left section */}
         
                         <div className='px-4 pr-10 text-black'>
-                            <p className='text-2xl mb-3'>Our Vision</p>
+                            <p className='text-2xl mb-3'>Mission</p>
 
-                            <p className='text-xs text-gray-600 mb-7'>Our Vision is to create a world class institution where the underprivileged children, considered as liabilities to the society are transformed into the assets of the society. We aim to cultivate self-sufficient individuals and community leaders who will shape the society positively for generations to come.</p>
-                            <p className='text-xs text-gray-600'>We also aim to create a platform that bridges the educational gap between privileged and underprivileged students, fostering true educational equity. Our goal is to provide a space where students from all socioeconomic backgrounds can learn and thrive together.</p>
-                            <div className='h-75 max-w-130 my-6'>
-                                <Image src='/assets/gallery1.jpg' alt='Gallery image 1' width={600} height={400} className='w-full h-full object-cover' />
-                            </div>
-
-                            <p className='text-2xl'>Our Mission</p>
-
-                            <p className='text-xs text-gray-600 mt-7'>We are on a mission to end generational poverty through modern and contemporary education. Committed to empowering orphans, destitute children, underprivileged students, and those from diverse socioeconomic backgrounds, we provide access to quality education based on the CBSE curriculum, modern pedagogy, and STEM learning. Through a nurturing environment focused on confidence-building, holistic development, and 21st-century skills, we will equip them to become future leaders, guided by strong moral and ethical values rooted in Islamic doctrine.</p>
-                            <p className='text-2xl my-4 '>Our Principle</p>
-
-                            <p className='text-xs text-gray-600'>We firmly believe that what we do at Umeed Global School has nothing philanthropic about it. It is a matter of mutual interest where we enable our children to become leaders who shall be capable and willing to make more leaders like themselves, in the future. Our children and we are partners in this cause.</p>
-                    
+                            <p className='text-sm font-semibold mb-3 text-gray-600 '>To ensure every child receives the education and care they deserve <br/> nurturing not just their minds but their future.</p>
+                            
                             <Socialmediabox/>
         
                         </div>
@@ -36,9 +25,9 @@ function Page() {
                         {/* right section */}
                         <div className='px-12 lg:px-0 flex justify-center lg:block mt-20 lg:mt-0 mb-3 lg:mb-0 w-full '>
                             <div className='w-full md:max-w-90 flex flex-col'>
-                                <Image src='/assets/gallery2.jpg' alt='Gallery image 1' width={600} height={400} className='w-full h-full object-cover' />
-                                <div className='py-7 px-10 shadow-[0_5px_25px_0_gray]'>
-                                    <button className='text-gray-700 text-xs py-3 w-full bg-yellow-500 rounded-sm relative top-0 hover:-top-2 transition-all duration-400 cursor-pointer'>DONATE NOW</button>
+                                <Image src='/assets/gallery2.jpg' alt='Gallery image 1' width={600} height={400} className='w-full h-full object-cover rounded-t-lg' />
+                                <div className='py-7 px-10 shadow-[0_5px_25px_0_gray] rounded-b-lg'>
+                                   <Link href={'/donate-us'}> <button className='text-gray-700 text-xs py-3 w-full bg-yellow-500 rounded-sm relative top-0 hover:-top-2 transition-all duration-400 cursor-pointer'>DONATE NOW</button> </Link>
                                 </div>
                             </div>
         
